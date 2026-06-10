ecd75b7a5926fa3ca72e1f00055dce24e49f68ef
Sonali <sonalilavate7@gmail.com>
2026-06-10 19:02:08 +0530

    Initial Spring Boot project setup

---
+++ smart-complaint-management-system/src/main/java/com/scms/SmartComplaintManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.scms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartComplaintManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartComplaintManagementSystemApplication.class, args);
+	}
+
+}
